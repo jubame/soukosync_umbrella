6a3783fc583013726627bdc011476ccd901dba01
J <j@localhost.localdomain>
2020-08-04 13:18:25 +0200

    Prevent startup user DB insertion on the test environment, as it messes up the 'list_users/0 returns all users' test.

--- apps/soukosync/lib/soukosync/caller.ex
+++ apps/soukosync/lib/soukosync/caller.ex
@@ -14,7 +14,11 @@ defmodule Soukosync.Caller do
   end
 
   def init(_) do
-    { :ok, Accounts.get_and_upsert_current_user }
+    if Mix.env == :test do
+      { :ok, Accounts.get_current_user }
+    else
+      { :ok, Accounts.get_and_upsert_current_user }
+    end
   end
 
   def handle_cast(:sync_user_warehouses, current_user) do
