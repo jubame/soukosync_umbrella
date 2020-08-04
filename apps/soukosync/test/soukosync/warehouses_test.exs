7e7f133a4ca28de85f373641d8e0aa0f2eb2153f
J <j@localhost.localdomain>
2020-08-04 11:51:09 +0200

    Related to previous commit: refactor tests

--- apps/soukosync/test/soukosync/warehouses_test.exs
+++ apps/soukosync/test/soukosync/warehouses_test.exs
@@ -1,10 +1,10 @@
 defmodule Soukosync.SyncTest do
   use Soukosync.DataCase
 
-  alias Soukosync.Sync
+  alias Soukosync.Warehouses
 
   describe "warehouses" do
-    alias Soukosync.Sync.Warehouse
+    alias Soukosync.Warehouses.Warehouse
 
     @valid_attrs %{city: "some city", country: "some country", fax: "some fax", line1: "some line1", line2: "some line2", name: "some name", phone: "some phone", site: "some site", state: "some state", zip_code: "some zip_code"}
     @update_attrs %{city: "some updated city", country: "some updated country", fax: "some updated fax", line1: "some updated line1", line2: "some updated line2", name: "some updated name", phone: "some updated phone", site: "some updated site", state: "some updated state", zip_code: "some updated zip_code"}
@@ -14,23 +14,23 @@ defmodule Soukosync.SyncTest do
       {:ok, warehouse} =
         attrs
         |> Enum.into(@valid_attrs)
-        |> Sync.create_warehouse()
+        |> Warehouses.create_warehouse()
 
       warehouse
     end
 
     test "list_warehouses/0 returns all warehouses" do
       warehouse = warehouse_fixture()
-      assert Sync.list_warehouses() == [warehouse]
+      assert Warehouses.list_warehouses() == [warehouse]
     end
 
     test "get_warehouse!/1 returns the warehouse with given id" do
       warehouse = warehouse_fixture()
-      assert Sync.get_warehouse!(warehouse.id) == warehouse
+      assert Warehouses.get_warehouse!(warehouse.id) == warehouse
     end
 
     test "create_warehouse/1 with valid data creates a warehouse" do
-      assert {:ok, %Warehouse{} = warehouse} = Sync.create_warehouse(@valid_attrs)
+      assert {:ok, %Warehouse{} = warehouse} = Warehouses.create_warehouse(@valid_attrs)
       assert warehouse.city == "some city"
       assert warehouse.country == "some country"
       assert warehouse.fax == "some fax"
@@ -44,12 +44,12 @@ defmodule Soukosync.SyncTest do
     end
 
     test "create_warehouse/1 with invalid data returns error changeset" do
-      assert {:error, %Ecto.Changeset{}} = Sync.create_warehouse(@invalid_attrs)
+      assert {:error, %Ecto.Changeset{}} = Warehouses.create_warehouse(@invalid_attrs)
     end
 
     test "update_warehouse/2 with valid data updates the warehouse" do
       warehouse = warehouse_fixture()
-      assert {:ok, %Warehouse{} = warehouse} = Sync.update_warehouse(warehouse, @update_attrs)
+      assert {:ok, %Warehouse{} = warehouse} = Warehouses.update_warehouse(warehouse, @update_attrs)
       assert warehouse.city == "some updated city"
       assert warehouse.country == "some updated country"
       assert warehouse.fax == "some updated fax"
@@ -64,19 +64,19 @@ defmodule Soukosync.SyncTest do
 
     test "update_warehouse/2 with invalid data returns error changeset" do
       warehouse = warehouse_fixture()
-      assert {:error, %Ecto.Changeset{}} = Sync.update_warehouse(warehouse, @invalid_attrs)
-      assert warehouse == Sync.get_warehouse!(warehouse.id)
+      assert {:error, %Ecto.Changeset{}} = Warehouses.update_warehouse(warehouse, @invalid_attrs)
+      assert warehouse == Warehouses.get_warehouse!(warehouse.id)
     end
 
     test "delete_warehouse/1 deletes the warehouse" do
       warehouse = warehouse_fixture()
-      assert {:ok, %Warehouse{}} = Sync.delete_warehouse(warehouse)
-      assert_raise Ecto.NoResultsError, fn -> Sync.get_warehouse!(warehouse.id) end
+      assert {:ok, %Warehouse{}} = Warehouses.delete_warehouse(warehouse)
+      assert_raise Ecto.NoResultsError, fn -> Warehouses.get_warehouse!(warehouse.id) end
     end
 
     test "change_warehouse/1 returns a warehouse changeset" do
       warehouse = warehouse_fixture()
-      assert %Ecto.Changeset{} = Sync.change_warehouse(warehouse)
+      assert %Ecto.Changeset{} = Warehouses.change_warehouse(warehouse)
     end
   end
 end
