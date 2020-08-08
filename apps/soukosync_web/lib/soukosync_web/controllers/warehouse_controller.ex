c2ceba209b3569dced39b282f280adb499616683
J <j@localhost.localdomain>
2020-08-08 11:29:01 +0200

    Refactor here due to previous refactor in soukosync app

--- apps/soukosync_web/lib/soukosync_web/controllers/warehouse_controller.ex
+++ apps/soukosync_web/lib/soukosync_web/controllers/warehouse_controller.ex
@@ -1,18 +1,18 @@
 defmodule SoukosyncWeb.WarehouseController do
   use SoukosyncWeb, :controller
 
-  alias Soukosync.Sync
-  alias Soukosync.Sync.Warehouse
+  alias Soukosync.Warehouses
+  alias Soukosync.Warehouses.Warehouse
 
   action_fallback SoukosyncWeb.FallbackController
 
   def index(conn, _params) do
-    warehouses = Sync.list_warehouses()
+    warehouses = Warehouses.list_warehouses()
     render(conn, "index.json", warehouses: warehouses)
   end
 
   def create(conn, %{"warehouse" => warehouse_params}) do
-    with {:ok, %Warehouse{} = warehouse} <- Sync.create_warehouse(warehouse_params) do
+    with {:ok, %Warehouse{} = warehouse} <- Warehouses.create_warehouse(warehouse_params) do
       conn
       |> put_status(:created)
       |> put_resp_header("location", Routes.warehouse_path(conn, :show, warehouse))
@@ -21,22 +21,22 @@ defmodule SoukosyncWeb.WarehouseController do
   end
 
   def show(conn, %{"id" => id}) do
-    warehouse = Sync.get_warehouse!(id)
+    warehouse = Warehouses.get_warehouse!(id)
     render(conn, "show.json", warehouse: warehouse)
   end
 
   def update(conn, %{"id" => id, "warehouse" => warehouse_params}) do
-    warehouse = Sync.get_warehouse!(id)
+    warehouse = Warehouses.get_warehouse!(id)
 
-    with {:ok, %Warehouse{} = warehouse} <- Sync.update_warehouse(warehouse, warehouse_params) do
+    with {:ok, %Warehouse{} = warehouse} <- Warehouses.update_warehouse(warehouse, warehouse_params) do
       render(conn, "show.json", warehouse: warehouse)
     end
   end
 
   def delete(conn, %{"id" => id}) do
-    warehouse = Sync.get_warehouse!(id)
+    warehouse = Warehouses.get_warehouse!(id)
 
-    with {:ok, %Warehouse{}} <- Sync.delete_warehouse(warehouse) do
+    with {:ok, %Warehouse{}} <- Warehouses.delete_warehouse(warehouse) do
       send_resp(conn, :no_content, "")
     end
   end
